293210977da1043fd6645a3f8f46d8fffe1a0101
Jaime Escoto <jaime.escoto@hotmail.com>
2017-11-01 16:27:32 -0600

    First Examples
    Sending first examples

---
+++ Exercises/hola mundo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Ejercicio Hola Mundo </title>
+  </head>
+  <body>
+    <h1>Ejercicio Hola Mundo </h1>
+    <h2>Mi primer ejercicio en PHP</h2>
+    <?php
+      echo "Hola Mundo";
+    ?>
+  </body>
+</html>
